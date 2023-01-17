7d5a2e2bbeee4848d7811e339d580533336b5960
Alhassan Osman <oalhassan847@gmail.com>
2023-01-17 16:58:06 +0000

    fix nav item functions

--- index.js
+++ index.js
@@ -1,4 +1,41 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const booksLink = document.querySelector('.list');
+    const formsLink = document.querySelector('.add-new');
+    const contactsLink = document.querySelector('.contact-link');
+    const booksPage = document.querySelector('.book-list');
+    const formsPage = document.querySelector('.form');
+    const contactsPage = document.querySelector('.contact');
+    const booksTitle = document.querySelector('.books-head');
+    formsPage.classList.add('hide');
+    contactsPage.classList.add('hide');
+    function showListOnly() {
+      formsPage.classList.add('hide');
+      contactsPage.classList.add('hide');
+      booksPage.classList.remove('hide');
+      booksTitle.classList.remove('hide');
+    }
+    booksLink.addEventListener('click', showListOnly);
+    function showContactOnly() {
+      booksPage.classList.add('hide');
+      formsPage.classList.add('hide');
+      booksTitle.classList.add('hide');
+      contactsPage.classList.remove('hide');
+    }
+    contactsLink.addEventListener('click', showContactOnly);
+    function showFormOnly() {
+      booksPage.classList.add('hide');
+      contactsPage.classList.add('hide');
+      booksTitle.classList.add('hide');
+      formsPage.classList.remove('hide');
+    }
+    formsLink.addEventListener('click', showFormOnly);
+  });
+
+
 import Book from "./modules/Book.js";
+
+const date = new Date().toDateString();
+document.getElementById('date').innerHTML = date;
 
   const addButton = document.querySelector('#add-book');
   const removeButton = document.querySelectorAll('.remove');
